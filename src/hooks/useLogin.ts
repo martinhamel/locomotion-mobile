decd1d72b4599f4fa2dd18606090d1cd3af7c1bd
Martin Hamel <martin@komunide.com>
2021-10-12 00:59:39 +0000

    removed toast

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import { Dispatch, SetStateAction, useContext, useEffect } from "react";
 import { AppContext } from "../../App";
-import Toast from 'react-native-simple-toast';
 
 const useLogin = (navigation) => {
   const { setTokens } = useContext(AppContext) as AppContextType;
@@ -16,11 +15,9 @@ const useLogin = (navigation) => {
           rememberMe: false,
         }
       );
-      Toast.show("connexion réussie")
       setTokens(tokens);
       navigation.push("Home")
     } catch (e) {
-      Toast.show("connexion échouée")
       console.log(e);
     }
   };
