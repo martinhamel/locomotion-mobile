5822c0c6ffaff4a6eac64d741715281c469db1fc
Martin Hamel <martin@komunide.com>
2021-10-22 14:38:36 +0000

    we have a map in reserve

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:16
+FROM node:14
 WORKDIR /workspace
 
 RUN npm i -g expo-cli local-cors-proxy prettier
 
-CMD bash -c "npm i && expo start"
+CMD bash -c "npm i && lcp --proxyUrl https://api.locomotion.app & expo start"
--- src/Reserve.tsx
+++ src/Reserve.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import useLoanables from "./hooks/useLoanables";
+import MapView from 'react-native-maps';
 
 const Reserve = () => {
   const loanables = useLoanables();
   return (
-    <View>
-      <Text>Réservation</Text>
+    <View style={styles.container}>
+       <MapView style={styles.map}/>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default Reserve;
